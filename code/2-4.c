acb611f7fbf23cd42f006025d107706770b58f1b
KimJeeHun1 <iopuioyui@naver.com>
2026-05-22 09:48:34 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -5,8 +5,16 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
-
-
+    char alphabet= 'a';
+    int count;
+    while(alphabet<='z'){
+        count=0;
+        while(feof(fp)!=0){
+            if(fgets(fp)==alphabet)
+                count++;
+        }
+        printf("%c: %d\n",alphabet, count);
+    }
 
     fclose(fp);
 }
