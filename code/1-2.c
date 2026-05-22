9669a95af272206d2eb523c7a14c4b550224b1f7
KimJeeHun1 <iopuioyui@naver.com>
2026-05-22 09:05:34 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, const char* argv[]){
+  if(argc <2)
+    return 0;
+
+  for(){
+    const char* filename = argc[i];
+    char buffer[256] = {0};
+
+    
+  }
+}
