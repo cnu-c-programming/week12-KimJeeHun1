9ab8278fa2b4c6fb3c96246ab82ec1e0ee479ba9
KimJeeHun1 <iopuioyui@naver.com>
2026-05-22 09:52:45 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,14 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+FILE* fp_src = fopen(argv[1],"r");
+FILE* fp_dst = fopen(argv[2],"w");
 
+FILE* temp = fopen("myfile2.txt", "rw");
+temp=fp_src;
+fp_dst=temp;
+    
+fclose(fp_src);
+fclose(fp_dst);
 }
 
