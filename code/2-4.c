53e2d4b0778bd2ab5371490f53f66934c764a9c1
KimJeeHun1 <iopuioyui@naver.com>
2026-05-27 07:33:12 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -5,15 +5,14 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
-    char alphabet= 'a';
-    int count;
-    while(alphabet<='z'){
-        count=0;
-        while(feof(fp)!=0){
-            if(fgets(fp)==alphabet)
-                count++;
+    for(char i='a'; i<='z'; i++){
+        int num=0;
+        char ch;
+        while((ch=fgetc(fp))!=EOF){
+            if(ch==i)
+                num++;
         }
-        printf("%c: %d\n",alphabet, count);
+        printf("%c: %d\n",i,num);
     }
 
     fclose(fp);
